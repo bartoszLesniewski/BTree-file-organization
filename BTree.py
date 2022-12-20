d1be6038da3b06c77bc18d96eba062b15b8ef9a4
Bartosz Leśniewski <blesniewski@outlook.com>
2022-12-21 00:43:42 +0100

    Compensation for non-leaf nodes works

--- BTree.py
+++ BTree.py
@@ -32,6 +32,7 @@ class BTree:
             root_node.records.append(record)
             root_node.pointers.append(previous_root.page_number)
             root_node.pointers.append(new_child_pointer)
+            self.update_parent(root_node)
 
         self.filesHandler.save_index_page(root_node)  # change it
         return root_node
@@ -39,41 +40,44 @@ class BTree:
     def insert_into_node(self, record, node):
         if not node.is_leaf():
             i = self.find_position(node, record)
-
             if i is None:
                 raise Exception("Record already exists!")
 
             record, new_child_pointer = self.insert_into_node(record,
                                                               self.filesHandler.load_index_page(node.pointers[i]))
 
-            # if after insertion the key to child, there is no new child (not split), we don't have to modify parent
+            # if there was split when inserting record to the child, we have to add a new record to its parent
+            # otherwise, we don't have to modify parent
             if new_child_pointer:
-                # otherwise we have to add new key from child to parent
+                # parent isn't full
                 if len(node.records) < 2 * self.d:
                     node.records.insert(i, record)
                     node.pointers.insert(i + 1, new_child_pointer)
                     self.filesHandler.save_index_page(node)  # change it
-                    # new_child_pointer = None  # return ??
 
                 # parent is full
                 else:
-                    # maybe add record and pointer?
-                    record, new_child_pointer = self.split(node, i, record, new_child_pointer)
-                    return record, new_child_pointer
+                    can_compensation = self.try_compensation4(node, record, new_child_pointer)
+                    if not can_compensation:
+                        record, new_child_pointer = self.split(node, i, record, new_child_pointer)
+                        return record, new_child_pointer
 
-        # node is leaf
+        # leaf node
         else:
             i = self.find_position(node, record)
             if len(node.records) < 2 * self.d:
                 node.records.insert(i, record)
                 self.filesHandler.save_index_page(node)  # change it
             else:
-                record, new_child_pointer = self.split(node, i, record)
-                return record, new_child_pointer
+                can_compensation = self.try_compensation4(node, record)
+                if not can_compensation:
+                    record, new_child_pointer = self.split(node, i, record)
+                    return record, new_child_pointer
 
         return None, None
 
-    def find_position(self, node, record):
+    @staticmethod
+    def find_position(node, record):
         i = len(node.records) - 1
         while i >= 0 and record[0] < node.records[i][0]:
             i -= 1
@@ -84,15 +88,58 @@ class BTree:
         else:
             return i
 
+    def try_compensation4(self, node, record, pointer=None):
+        can_compensate = False
+        if node.parent_page:
+            parent_node = self.filesHandler.load_index_page(node.parent_page)
+            index = parent_node.pointers.index(node.page_number)
+            if index - 1 >= 0:
+                left_neighbour = self.filesHandler.load_index_page(parent_node.pointers[index - 1])
+                if len(left_neighbour.records) < 2 * self.d:
+                    self.compensation(left_neighbour, node, parent_node, index - 1, record, pointer)
+                    can_compensate = True
+            if not can_compensate and index + 1 < len(parent_node.pointers):
+                right_neighbour = self.filesHandler.load_index_page(parent_node.pointers[index + 1])
+                if len(right_neighbour.records) < 2 * self.d:
+                    self.compensation(node, right_neighbour, parent_node, index, record, pointer)
+                    can_compensate = True
+
+        return can_compensate
+
+    def compensation(self, left_child, right_child, parent, i, record, pointer=None):
+        # pointer parameter is necessary only for non-leaf nodes
+        records_distribution_list = left_child.records + [parent.records[i]] + right_child.records
+
+        j = len(records_distribution_list) - 1
+        while j >= 0 and record[0] < records_distribution_list[j][0]:
+            j -= 1
+        j += 1
+        records_distribution_list.insert(j, record)
+        middle = len(records_distribution_list) // 2
+
+        left_child.records = records_distribution_list[0:middle]
+        right_child.records = records_distribution_list[middle + 1:]
+        parent.records[i] = records_distribution_list[middle]
+
+        if not left_child.is_leaf():
+            pointers_distribution_list = left_child.pointers + right_child.pointers
+            pointers_distribution_list.insert(j + 1, pointer)
+            left_child.pointers = pointers_distribution_list[0:middle + 1]
+            right_child.pointers = pointers_distribution_list[middle + 1:]
+            self.update_parent(left_child)
+            self.update_parent(right_child)
+            print("AAAAA NOT LEAF ;-;")
+
+        self.filesHandler.save_index_page(left_child)  # change it
+        self.filesHandler.save_index_page(right_child)  # change it
+        self.filesHandler.save_index_page(parent)  # change it
 
     def split(self, node, index, record, pointer=None):
-        # son - overflown node
-        # index - position of the son node in parent node
-
         new_node = self.filesHandler.create_new_index_page()
         middle = self.d - 1
         record_for_parent = node.records[middle]
         new_node.records = node.records[middle + 1:]
+        new_node.parent_page = node.parent_page  # set the same parent by default, it will be changed if it is necessary
         # parent_node.pointers.insert(index + 1, new_node.page_number)
         # parent_node.records.insert(index, son_node.records[middle])
         node.records = node.records[0:middle]
@@ -117,75 +164,13 @@ class BTree:
 
         self.filesHandler.save_index_page(old_node)  # change it
         self.filesHandler.save_index_page(new_node)  # change it
+        self.update_parent(new_node)
 
-    def insert_non_full(self, node, record):
-        i = len(node.records) - 1
-        while i >= 0 and record[0] < node.records[i][0]:
-            i -= 1
-        i += 1
-
-        if node.is_leaf():
-            node.records.insert(i, record)
-            self.filesHandler.save_index_page(node)
-        else:
-            child = self.filesHandler.load_index_page(node.pointers[i])
-            if len(child.records) == 2 * self.d:
-                # fix it - save information about number of keys on the page
-                can_compensate = False
-                if i - 1 >= 0 and child.is_leaf():
-                    left_neighbour = self.filesHandler.load_index_page(node.pointers[i - 1])
-                    if len(left_neighbour.records) < 2 * self.d:
-                        self.compensation(left_neighbour, child, node, i - 1, record)
-                        can_compensate = True
-                if not can_compensate and i + 1 < len(node.pointers) and child.is_leaf():
-                    right_neighbour = self.filesHandler.load_index_page(node.pointers[i + 1])
-                    if len(right_neighbour.records) < 2 * self.d:
-                        self.compensation(child, right_neighbour, node, i, record)
-                        can_compensate = True
-                if not can_compensate:
-                    self.split_child(node, i, child)
-
-                    if record[0] > node.records[i][0]:
-                        child = self.filesHandler.load_index_page(node.pointers[i + 1])
-
-                    self.insert_non_full(child, record)
-            else:
-                self.insert_non_full(child, record)
-
-    def compensation(self, left_neighbour, right_neighbour, ancestor, i, record):
-        # records_distribution_list = left_neighbour.records + [ancestor.records[i]] + right_neighbour.records
-        # pointers_distribution_list = left_neighbour.pointers + right_neighbour.pointers
-        # partition = len(records_distribution_list) // 2
-
-        records_distribution_list = left_neighbour.records + [ancestor.records[i]] + right_neighbour.records
-
-        j = len(records_distribution_list) - 1
-        while j >= 0 and record[0] < records_distribution_list[j][0]:
-            j -= 1
-
-        records_distribution_list.insert(j + 1, record)
-        pointers_distribution_list = left_neighbour.pointers + right_neighbour.pointers
-        middle = len(records_distribution_list) // 2
-
-        left_neighbour.records = records_distribution_list[0:middle]
-        right_neighbour.records = records_distribution_list[middle + 1:]
-        ancestor.records[i] = records_distribution_list[middle]
-
-        if not left_neighbour.is_leaf():
-            left_neighbour.pointers = pointers_distribution_list[0:middle + 1]
-            right_neighbour.pointers = pointers_distribution_list[middle + 1:]
-            print("AAAAA NOT LEAF ;-;")
-
-        # records_distribution_list[middle], ancestor.records[i] = ancestor.records[i], records_distribution_list[middle]
-        # left_neighbour.records = records_distribution_list[0:middle]  # exclude middle
-        # left_neighbour.pointers = pointers_distribution_list[0:middle + 1]  # exclude middle + 1
-        # right_neighbour.records = records_distribution_list[middle:]  # include middle
-        # right_neighbour.pointers = pointers_distribution_list[middle + 1:]  # include middle + 1
-        # ancestor.records[i] = records_distribution_list[partition]
-
-        self.filesHandler.save_index_page(left_neighbour)
-        self.filesHandler.save_index_page(right_neighbour)
-        self.filesHandler.save_index_page(ancestor)
+    def update_parent(self, parent_node):
+        for child_page in parent_node.pointers:
+            child_node = self.filesHandler.load_index_page(child_page)
+            child_node.parent_page = parent_node.page_number
+            self.filesHandler.save_index_page(child_node)
 
     def print(self, print_records=False):
         if self.root_page is not None:
@@ -236,19 +221,3 @@ class BTree:
                 return False
 
             return self.search(key, node.pointers[i])
-
-    def try_compensation(self, node, parent_node, i, record):
-        can_compensate = False
-        i = parent_node.pointers.index(node.page_number)
-        if i - 1 >= 0 and node.is_leaf():
-            left_neighbour = self.filesHandler.load_index_page(parent_node.pointers[i - 1])
-            if len(left_neighbour.records) < 2 * self.d:
-                self.compensation(left_neighbour, node, parent_node, i - 1, record)
-                can_compensate = True
-        if not can_compensate and i + 1 < len(parent_node.pointers) and node.is_leaf():
-            right_neighbour = self.filesHandler.load_index_page(parent_node.pointers[i + 1])
-            if len(right_neighbour.records) < 2 * self.d:
-                self.compensation(node, right_neighbour, parent_node, i, record)
-                can_compensate = True
-
-        return can_compensate
